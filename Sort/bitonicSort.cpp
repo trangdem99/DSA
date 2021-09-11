d2e59fda5ab2e7c1e1039323af1745a1963ecdf9
trangdem99 <84564250+trangdem99@users.noreply.github.com>
2021-09-11 15:17:08 +0700

    Add files via upload

---
+++ Sort/bitonicSort.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+using namespace std;
+
+void printData(int* data, int size) {
+	for (int i = 0; i < size; ++i) {
+		cout << "  " << data[i];
+	}
+	cout << endl;
+}
+
+void swap(int& a, int& b) {
+	int temp = a;
+	a = b;
+	b = temp;
+}
+
+/*The parameter dir indicates the sorting direction, ASCENDING or DESCENDING; if (a[i] > a[j]) agrees with the direction, then a[i] and a[j] are interchanged.*/
+void compAndSwap(int* data, int i, int j, int dir) {
+	if (dir == (data[i] > data[j]))
+		swap(data[i], data[j]);
+}
+
+/*It recursively sorts a bitonic sequence in ascending order, if dir = 1, and in descending order otherwise (means dir=0). The sequence to be sorted starts at index position low, the parameter cnt is the number of elements to be sorted.*/
+void bitonicMerge(int* data, int low, int cnt, int dir) {
+	if (cnt > 1) {
+		int k = cnt / 2;
+		for (int i = low; i < low + k; i++)
+			compAndSwap(data, i, i + k, dir);
+		bitonicMerge(data, low, k, dir);
+		bitonicMerge(data, low + k, k, dir);
+	}
+}
+
+/* This function first produces a bitonic sequence by recursively sorting its two halves in opposite sorting orders, and then calls bitonicMerge to make them in the same order */
+void bitonicSort(int* data, int low, int cnt, int dir) {
+	if (cnt > 1) {
+		int k = cnt / 2;
+
+		// sort in ascending order since dir here is 1
+		bitonicSort(data, low, k, 1);
+
+		// sort in descending order since dir here is 0
+		bitonicSort(data, low + k, k, 0);
+
+		// Will merge whole sequence in ascending order since dir=1.
+		bitonicMerge(data, low, cnt, dir);
+	}
+}
+
+int main() {
+	int size;
+	cout << "Input size: "; cin >> size;
+
+	int* data = new int[size];
+
+	for (int i = 0; i < size; i++) {
+		cout << "Input element number " << i + 1 << " : ";
+		cin >> data[i];
+	}
+
+	bitonicSort(data, 0, size, 1);
+
+	cout << "Sorted Array in Ascending Order:" << endl;
+
+	printData(data, size);
+
+	return 0;
+}
---
+++ Sort/cycleSort.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+
+using namespace std;
+
+void printData(int* data, int size) {
+	for (int i = 0; i < size; ++i) {
+		cout << "  " << data[i];
+	}
+	cout << endl;
+}
+
+void swap(int& a, int& b) {
+	int temp = a;
+	a = b;
+	b = temp;
+}
+
+void cycleSort(int* data, int n)
+{
+	int writes = 0;
+
+	// traverse array elements and put it to on the right place
+	for (int c_start = 0; c_start <= n - 2; c_start++) {
+		int item = data[c_start];
+
+		// Find position where we put the item.
+		int pos = c_start;
+		for (int i = c_start + 1; i < n; i++)
+			if (data[i] < item)
+				pos++;
+
+		if (pos == c_start)
+			continue;
+
+		while (item == data[pos])
+			pos += 1;
+
+		if (pos != c_start) {
+			swap(item, data[pos]);
+			writes++;
+		}
+
+		// Rotate rest of the cycle
+		while (pos != c_start) {
+			pos = c_start;
+
+			for (int i = c_start + 1; i < n; i++)
+				if (data[i] < item)
+					pos += 1;
+
+			while (item == data[pos])
+				pos += 1;
+
+			if (item != data[pos]) {
+				swap(item, data[pos]);
+				writes++;
+			}
+		}
+	}
+}
+
+int main() {
+	int size;
+	cout << "Input size: "; cin >> size;
+
+	int* data = new int[size];
+
+	for (int i = 0; i < size; i++) {
+		cout << "Input element number " << i + 1 << " : ";
+		cin >> data[i];
+	}
+
+	cycleSort(data, size);
+
+	cout << "Sorted Array in Ascending Order:" << endl;
+
+	printData(data, size);
+
+	return 0;
+}
